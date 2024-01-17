e090052a77d1508da3f64ce62233022215738463
RomanKosmyna <roma.kosmyna@gmail.com>
2024-01-17 11:18:02 +0200

    Added a Timer class and created an instance of it in Program class.

--- PomodoroTimer/Program.cs
+++ PomodoroTimer/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace PomodoroTimer;
+
+class Program
+{
+    static void Main()
+    {
+        Timer timer = new();
+        timer.SetTimer();
+
+        Console.ReadKey();
+    }
+}
---
+++ PomodoroTimer/Timer.cs
@@ -0,0 +1,20 @@
+﻿namespace PomodoroTimer;
+
+internal class Timer
+{
+    private static System.Timers.Timer _timer;
+    private readonly int _interval = 30000;
+
+    public void SetTimer()
+    {
+        _timer = new System.Timers.Timer(_interval);
+        _timer.Elapsed += OnTimedEvent;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+    }
+
+    public void OnTimedEvent(object sender, EventArgs e)
+    {
+        Console.Beep();
+    }
+}
